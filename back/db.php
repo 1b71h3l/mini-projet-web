c3de55a747b1fa4f11d732f5e01b61088a185cb0
IbtihelHT <hattabiibtihel@gmail.com>
2023-01-04 13:36:34 +0100

    tootlip telecharger

--- back/db.php
+++ back/db.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$db="mini_projet_web";
+	$db="iot";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password,$db);
 
